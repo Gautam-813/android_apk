0d316e942b5103311cd11f1cf52af666d5824f85
Gautam-813 <145947168+Gautam-813@users.noreply.github.com>
2025-07-28 13:35:04 +0530

    Add files via upload

---
+++ final_apk/app/src/main/java/com/whispersphere/app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.whispersphere.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        
+        webView.loadUrl("file:///android_asset/app.html");
+    }
+}
